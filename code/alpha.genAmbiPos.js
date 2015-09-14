61e38d5b2f9162bcdf00ea52d98129ef43540f4f
ATK! <contact@atk.io>
2015-09-14 14:44:05 +0200

    Import Audio Rohzlas

--- code/alpha.genAmbiPos.js
+++ code/alpha.genAmbiPos.js
@@ -2,10 +2,20 @@ inlets =1;
 outlets = 1;
 autowatch = 1;
 
+var half = 0;
+
+function halfCircle(val){
+		half = val;
+}
+
 function msg_int(val){
 	outlet(0, "clear");
 	for(i = 0; i < val; i++){
-		pos = 360 / val * i + 180 / val;
+		if(half == 0){
+			var pos = 360 / val * i + 180 / val - (360 / val);
+		} else {
+			var pos = (360 / val * i + 180 / val) / 2 - 90;
+		}		
 		outlet(0, "aed " + (i + 1) + " " + pos + " 0. 1.");
 	}
 }
